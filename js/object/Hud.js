4ce4d293d9b6a36dd98c19e72cadab93c5a26387
Yusuf Afandi <afandi.yusuf.04@gmail.com>
2015-09-23 15:44:34 +0700

    add hud for player score

--- js/object/Hud.js
+++ js/object/Hud.js
@@ -1,9 +1,13 @@
 var Hud = function(game,player){
 	
 	this.healTeks = game.add.text(10, 10, "100/100");
+
+	this.scoreText = game.add.text(10, 50, "Score is " + playerScore);
+
 	this.player = player;
 }
 
 Hud.prototype.update = function(){
 	this.healTeks.text = ""+this.player.currentHealth+" / "+this.player.maxHealth;
+	this.scoreText.text = "Score is " + playerScore;
 }
