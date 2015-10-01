22c267055787991543d0fa4261ffb3522c30f3ba
Yusuf Afandi <afandi.yusuf.04@gmail.com>
2015-10-01 22:36:20 +0700

    implement bitmap font

--- js/state/MainMenuState.js
+++ js/state/MainMenuState.js
@@ -1,17 +1,36 @@
 var mainMenuState = {
 	preload : function () {
 		game.load.spritesheet('button', 'assets/button/button_sprite_sheet.png', 193, 71);
-    
+    	game.load.image('background', 'assets/sprites/background.jpg');
+    	game.load.bitmapFont('fonts', 'font/fonts.png', 'font/fonts.xml');
 	},
 	create :function(){
+		transitionPlugin = game.plugins.add(Phaser.Plugin.StateTransition);
+
+		//define new properties to be tweened, duration, even ease
+		transitionPlugin.settings({
+
+		    //how long the animation should take
+		    duration: 1000,
+
+		    //ease property
+		    ease: Phaser.Easing.Exponential.InOut, /* default ease */
+
+		    //what property should be tweened
+		    properties: {
+		        alpha: 0
+		    }
+		});
+
+		game.add.sprite(0, 0, 'background');
 		this.scale.scaleMode = Phaser.ScaleManager.NO_SCALE;
 		this.scale.pageAlignHorizontally = true;
 		this.scale.pageAlignVertically = true;
 
-		button = game.add.button(game.world.centerX - 95, 400, 'button', actionOnClick, this, 2, 1, 0);
-		var style = { font: "32px Arial", fill: "#ff0044", wordWrap: true, wordWrapWidth: game.width - 100, align: "center" };
+		button = game.add.button(game.world.centerX - 95, 400, 'button', actionOnClick2, this, 2, 1, 0);
+		//var style = { font: "32px fonts", fill: "#ff0044", wordWrap: true, wordWrapWidth: game.width - 100, align: "center" };
 
-		var text = game.add.text(0, 0, "THE AMAZING GAME", style);
+		var text = game.add.bitmapText(0, 0,"fonts", "THIS IS TITLE",64);
 		text.x = game.world.centerX;
 		text.y = 300;
 		text.anchor.set(0.5);
@@ -22,7 +41,10 @@ var mainMenuState = {
 	}
 }
 
-function actionOnClick () {
-	console.log("goto ingame");
-    game.state.start("ingameState",true,false);
+function actionOnClick2 () {
+	//console.log("goto ingame");
+	//and later on
+	transitionPlugin.to("ingameState");
+
+    //game.state.start("ingameState",true,false);
 }
--- js/state/ResultState.js
+++ js/state/ResultState.js
@@ -1,10 +1,12 @@
 var resultState = {
 	preload : function () {
-		
+		game.load.image('background', 'assets/sprites/background.jpg');
 		game.load.spritesheet('button', 'assets/button/button_sprite_sheet.png', 193, 71);
+		game.load.bitmapFont('fonts', 'font/font.png', 'font/font.xml');
     
 	},
 	create :function(){
+		game.add.sprite(0, 0, 'background');
 		this.scale.scaleMode = Phaser.ScaleManager.NO_SCALE;
 		this.scale.pageAlignHorizontally = true;
 		this.scale.pageAlignVertically = true;
@@ -12,7 +14,8 @@ var resultState = {
 		button = game.add.button(game.world.centerX - 95, 400, 'button', actionOnClick, this, 2, 1, 0);
 		
 		var style = { font: "32px Arial", fill: "#ff0044", wordWrap: true, wordWrapWidth: game.width - 100, align: "center" };
-		var text = game.add.text(0, 0, "GAME END your score is " + playerScore, style);
+
+		var text = game.add.bitmapText(0, 0,"fonts", "GAME END your score is " + playerScore , 32);
 
 		text.x = game.world.centerX;
 		text.y = 300;
@@ -25,6 +28,5 @@ var resultState = {
 }
 
 function actionOnClick () {
-	
-    game.state.start("ingameState",true,false);
+	transitionPlugin.to("ingameState");
 }
